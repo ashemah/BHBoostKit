ce68b327949718e54ea4ab534e863ac78f190e2d
Ashemah Harrison <ashemah@boostedhuman.com>
2012-03-19 13:25:11 +1100

    Updated NIBTools

--- NIBTools/BHNIBTools.m
+++ NIBTools/BHNIBTools.m
@@ -3,7 +3,7 @@
 //  TradieOffice
 //
 //  Created by Ashemah Harrison on 1/8/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Boosted Human. All rights reserved.
 //
 
 #import "BHNIBTools.h"
